a5fa5a15cea8867082357ea89e09dbf87320cd88
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2022-09-10 03:40:33 +0530

    Time: 1613 ms (7.05%), Space: 125.1 MB (91.79%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     static bool cmp(vector<int> &a,vector<int> &b){
+        // reverse order of attack time if the elements are not equal
         if(a[0]!=b[0]){
-            return a[0]>b[0];
+           return a[0]>b[0];
         }
-        else{
-            return a[1]<b[1];
-        }
+        // agar equal hai to second waala chota pehle
+        return a[1]<b[1];
     }
-
     int numberOfWeakCharacters(vector<vector<int>>& properties) {
-        sort(properties.begin(),properties.end(),cmp);
-        int weak=0;
-        int curr=INT_MIN;
-        for(auto it: properties){
-            if(curr>it[1]){
-                weak+=1;
+        // sorting in reverse order of attack time
+        int n = properties.size();
+        sort(properties.begin(), properties.end(),cmp);
+        int max = INT_MIN;
+        int weak = 0;
+        for(int i =0;i<n;i++){
+            // agar max bada hai current element se to weak hai
+            if(max>properties[i][1]){
+                weak++;
             }
-            else{
-                curr=it[1];
-            }
+            else
+            // warna max ko update kardo
+                max = properties[i][1];
         }
+        
         return weak;
     }
 };
-
