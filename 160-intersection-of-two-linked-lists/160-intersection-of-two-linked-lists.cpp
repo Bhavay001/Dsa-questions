9d00546f5fe54e272c96d5d117bf69b56cdfde18
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2022-09-16 19:28:23 +0530

    Time: 84 ms (44.59%), Space: 14.7 MB (32.93%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,45 +9,30 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if(headA ==NULL || headB==NULL){
+        if(headA == NULL || headB == NULL){
             return NULL;
         }
+        // this approach now what we do is make curr1 point headA and curr2 to headB initially
+        // now we move forward both the curr1 and curr2 till they become equal
+        // if curr1 becomes null we make curr1 point to haadB
+        // if curr2 becomes null we make curr2 point to headA
+        // in the second iteration of curr's they come at same level/ means same distance between them
         ListNode * curr1 = headA;
         ListNode * curr2 = headB;
-        int countA = 0;
-        int countB = 0;
-        // first we find the size of the first and second list
-        while(curr1!=NULL){
-            countA++;
-            curr1 = curr1->next;
-        }
-        while(curr2!=NULL){
-            countB++;
-            curr2 = curr2->next;
-        }
-        curr1 = headA;
-        curr2 = headB;
-        // we find the absolute difference between the size of both the linked list
-        int d = abs(countA - countB);
-        // we run the loop d no of time and if size of 1st ll is greater we move its curr forward else we move the                  2nd lls curr forward
-        for(int i =0;i<d;i++){
-            if(countA> countB){
-                curr1 =curr1->next;
+        while(curr1 != curr2){
+            if(curr1 == NULL){
+                curr1 = headB;
             }
-            else if(countA<countB){
-                curr2 = curr2 ->next;
+            else{
+                curr1 = curr1 ->next;
             }
-            
-        }
-        // now we compare the nodes of first and second linked list if they are equal we break and return curr pointer
-        while(curr1!=NULL && curr2 !=NULL){
-            if(curr1 == curr2){
-                break;
+            if(curr2 == NULL){
+                curr2 = headA;
             }
-            curr1 = curr1->next;
-            curr2 = curr2 ->next;
+            else{
+                curr2 = curr2->next;
+            }
         }
         return curr1;
-       
     }
 };
