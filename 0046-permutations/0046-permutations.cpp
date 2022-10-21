4af1c18258116dd6146b8d917f271822848d35fa
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2022-10-21 22:15:13 +0530

    Time: 6 ms (52.26%), Space: 7.5 MB (91.80%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
     
-    void solve(int index , int count, vector<int> &nums,vector<vector<int>> &finalans, vector<int> &ans, vector<int> &visited,int n){
-        
-        if(count == n){
+    void solve(vector<int> &ans, vector<vector<int>> &finalans,vector<int> &visited, vector<int>&nums){
+        // base case when ans vector becomes equal the size of the origional vector
+        if(ans.size()==nums.size()){
             finalans.push_back(ans);
             return;
         }
-        
-        for(int i =0;i<n;i++){
-            if(visited[i]==0){
+        // we check the ans vector kif it is not visited we push it into our answer
+        for(int i =0;i<nums.size();i++){
+            if(!visited[i]){
                 ans.push_back(nums[i]);
-                count += 1;
                 visited[i] = 1;
-                solve(index+1,count, nums,finalans, ans, visited , n);
+                solve(ans,finalans, visited, nums);
                 ans.pop_back();
-                count -=1;
-                visited[i]= 0;
+                visited[i] = 0;
             }
         }
-    
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
-        
+        int n = nums.size();
         vector<vector<int>> finalans;
         vector<int> ans;
-        int index = 0;
-        int count = 0;
-        int n = nums.size();
-        vector<int> visited(n,0);
-        solve(index ,count ,nums,finalans, ans, visited ,n);
+        vector<int> visited(n, 0);
+        solve(ans, finalans , visited, nums);
         return finalans;
     }
 };
