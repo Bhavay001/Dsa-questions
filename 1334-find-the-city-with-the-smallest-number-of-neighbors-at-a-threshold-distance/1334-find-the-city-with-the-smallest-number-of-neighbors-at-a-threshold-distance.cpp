9304aa9fc070cd28f884e6dc91010d3359ac7fd6
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2022-12-11 14:22:27 +0530

    Time: 95 ms (84.97%), Space: 13.6 MB (27.71%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,41 +1,46 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> dist(n, vector<int> (n,INT_MAX));
+        vector<pair<int,int>> adj[n];
         
         // first we have to make initial matrix from the given
         for(auto x:edges){
             int u = x[0];
             int v = x[1];
             int wt = x[2];
-            dist[u][v] = wt;
-            dist[v][u] = wt;
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
         }
         
-        // for itself to itself
-        for(int i=0;i<n;i++){
-            dist[i][i] = 0;
-        }
-        
-        // now applying floyds warshall algo
-        for(int via=0;via<n;via++){
-            for(int i =0;i<n;i++){
-                for(int j =0;j<n;j++){
-                    if(dist[i][via]==INT_MAX || dist[via][j]==INT_MAX){
-                        continue;
-                    }
-                    dist[i][j] = min(dist[i][j],dist[i][via] + dist[via][j]);
-                }
-            }
-        }
-        
-        // here we just needed to store the smallest number of cities
+        priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>> pq;
         int citynum = -1;
         int maxcount = n;
-        for(int i = 0;i<n;i++){
+        
+        // now applying dijaktra's algo
+        for(int i =0;i<n;i++){
+            vector<int> distance(n,INT_MAX);
+            pq.push({0,i});
+            distance[i] = 0;
+            while(!pq.empty()){
+                int node = pq.top().second;
+                int wt = pq.top().first;
+                pq.pop();
+                
+                for(auto x:adj[node]){
+                    int newnode = x.first;
+                    int adjwt = x.second;
+                    
+                    if(distance[node] + adjwt < distance[newnode]){
+                        distance[newnode] = distance[node] + adjwt;
+                        pq.push({distance[newnode],newnode});
+                    }
+                }
+            }
+
+            // here we just needed to store the smallest number of cities
             int count = 0;
             for(int j =0;j<n;j++){
-                if(dist[i][j]<=distanceThreshold){
+                if(distance[j]<=distanceThreshold){
                     count++;
                 }
             }
@@ -44,6 +49,8 @@ public:
                 maxcount = count;
             }
         }
+            
+     
         return citynum;
     }
 };
