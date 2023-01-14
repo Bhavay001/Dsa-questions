9ce6ceb1f2b314b162390d6909f859f24a763ab5
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-01-15 00:04:33 +0530

    Time: 199 ms (5.06%), Space: 13.4 MB (10.17%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(char a , char b){
+        if(a>b){
+            return false;
+        }
+        return true;
+    }
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size();
+        int m = strs[0].size();
+        int ans = 0;
+        set<int> s;
+        unordered_map<int,char> mp;
+        for(int i =0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(mp.find(j)==mp.end()){
+                    mp[j] = strs[i][j];
+                    
+                }
+                else if(isValid(mp[j],strs[i][j])){
+                    mp[j] = strs[i][j];
+                }
+                else{
+                    cout<<strs[i][j];
+                    s.insert(j);
+                }
+            }
+        }
+        return s.size();
+    }
+};
+
+
