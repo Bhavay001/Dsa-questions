c2e3b8de66361906fe010fa622784b060e2cdf30
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-01-19 01:04:44 +0530

    Time: 64 ms (90.21%), Space: 40 MB (38.25%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        // two conditions are possible the maximum sum is not in circulaar but normal
+        // other is maximum sum is formed by circular sum
+        //to solve the first we can simply apply kadanes algo
+        // to solve the circular sum we need to find the minimum subarray and
+        // subract it from total sum
+       
+        int minsum = 0;
+        int maxsum = 0;
+        int totalsum = 0;
+        int maxi = INT_MIN;
+        int mini = INT_MAX;
+        for(int i =0;i<nums.size();i++){
+            totalsum += nums[i];
+            minsum +=nums[i];
+            maxsum += nums[i];
+            
+            maxi= max(maxsum , maxi);
+            if(maxsum<0){
+                maxsum = 0;
+            }
+            
+            mini = min(minsum , mini);
+            if(minsum>0){
+                minsum = 0;
+            }
+        }
+        int max1 = maxi;
+        int max2 = totalsum - mini;
+        if(totalsum == mini){
+            return maxi;
+        }
+        return max(max1,max2);
+    }
+};
