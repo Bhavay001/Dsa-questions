25ed46995fe8d40608e97d37a8fa12f91b93eacc
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-02-21 02:45:03 +0530

    Time: 6 ms (15.12%), Space: 8.1 MB (7.79%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    
+// this is the tabulation bottom-up approach of house roober just that it was given in a circle
+// so i first gave an array find maxsum where 1st element was not included
+//then found for array where last element is not present as both cannot be together in same ans
+// returned maximum among both the arrays 
     int solve(vector<int>& arr){
         int n = arr.size();
-        if(n==1){
-            return arr[0];
-        }
-        int prev2 = arr[0];
-        int prev =  max(arr[0],arr[1]);
+        vector<int> dp(n,-1);
+        dp[0] = arr[0];
+        dp[1] =  max(arr[0],arr[1]);
         
         for(int i =2;i<n;i++){
-            int pick = prev2 + arr[i];
-            int notpick = prev;
-            int curr = max(pick,notpick);
-            prev2 = prev;
-            prev = curr;
+            int pick = dp[i-2] + arr[i];
+            int notpick = dp[i-1];
+             dp[i] = max(pick,notpick);
         }
-        return prev;
+        return dp[n-1];
     }
     
     int rob(vector<int>& nums) {
