7358dc668aab4c1d1fee020389ee7072813da3e1
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-02-27 01:29:36 +0530

    Time: 7 ms (32.27%), Space: 7.8 MB (72.00%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -6,32 +6,33 @@ public:
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         
-        // tabulation bottom up
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        // space optimization
+        vector<int> prev(n,0);
         
         for(int i =0;i<m;i++){
+            vector<int> temp(n,0);
             for(int j =0;j<n;j++){
-                
                 if(obstacleGrid[i][j]==1){
-                    dp[i][j] = 0;;
+                    temp[j] = 0;;
                 }
                 else if(i==0 && j==0){
-                    dp[i][j] = 1;
+                    temp[j] = 1;
                 }
                 else{
                     int left = 0;
                     int up = 0;
                     if(i>0){
-                        left = dp[i-1][j];
+                        left = prev[j];
                     }
                     if(j>0){
-                        up = dp[i][j-1];
+                        up = temp[j-1];
                     } 
-                    dp[i][j] = left+up;
+                    temp[j] = left+up;
                 }
-
             }
+            prev = temp;
+
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
