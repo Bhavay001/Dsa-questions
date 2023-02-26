fabf345f0e93d1e12f557940ab21fe3e8894d2ab
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-02-26 23:58:46 +0530

    Time: 3 ms (78.66%), Space: 8 MB (16.92%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int solve(int m ,int n,vector<vector<int>>& obstacleGrid,vector<vector<int>> & dp){
+        // if we reach the start point and tha start point is not obstacle we return 1
         if(m==0 && n==0 && obstacleGrid[m][n]!=1){
             return 1;
         }
+        // we do not take the way if the way has obstacle
         if(m<0 || n<0 || obstacleGrid[m][n]==1){
             return 0;
         }
@@ -19,6 +21,9 @@ public:
         
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
+        
+        //memoization 
+        
         vector<vector<int>> dp(m,vector<int>(n,-1));
         return solve(m-1,n-1,obstacleGrid,dp);
     }
