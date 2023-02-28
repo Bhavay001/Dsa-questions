cd2c0f054f5e211fecfc5d1973fdf0d4c19904f5
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-02-28 12:20:58 +0530

    Time: 7 ms (68.14%), Space: 8.9 MB (55.65%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -16,9 +16,26 @@ public:
     }
     
     int minimumTotal(vector<vector<int>>& triangle) {
-        // memoization
+        // whatever is the recursion here from 0 -> n-1 
+        // the opposite will be tabulation
+        
         int n = triangle.size();
         vector<vector<int>> dp(n,vector<int>(n,-1));
-        return solve(0,0,triangle,dp);
+        // tabulation bottom up
+        // base case if i is n-1 j can be anything from 0 to n-1 in that row
+        // anywhere we reach in last row is a base case
+        for(int j =0;j<n;j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        // so tabulation will be opposite n-1 to 0
+        for(int i =n-2;i>=0;i--){
+            // if i is 2 j can be 0 1 2
+            for(int j =i;j>=0;j--){
+                int down = triangle[i][j] + dp[i+1][j];
+                int diagonal = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down, diagonal);
+            }
+        }
+        return dp[0][0];
     }
 };
