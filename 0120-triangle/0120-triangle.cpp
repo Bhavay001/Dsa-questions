6eef31fe83e0a7115e9221f4d4c005b4e378a785
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-02-28 12:43:51 +0530

    Time: 3 ms (97.43%), Space: 8.9 MB (55.65%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -20,22 +20,24 @@ public:
         // the opposite will be tabulation
         
         int n = triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        // tabulation bottom up
+        vector<int> prev(n,0);
+        // space optimization
         // base case if i is n-1 j can be anything from 0 to n-1 in that row
         // anywhere we reach in last row is a base case
         for(int j =0;j<n;j++){
-            dp[n-1][j] = triangle[n-1][j];
+            prev[j] = triangle[n-1][j];
         }
         // so tabulation will be opposite n-1 to 0
         for(int i =n-2;i>=0;i--){
+            vector<int> curr(n,0);
             // if i is 2 j can be 0 1 2
             for(int j =i;j>=0;j--){
-                int down = triangle[i][j] + dp[i+1][j];
-                int diagonal = triangle[i][j] + dp[i+1][j+1];
-                dp[i][j] = min(down, diagonal);
+                int down = triangle[i][j] + prev[j];
+                int diagonal = triangle[i][j] + prev[j+1];
+                curr[j] = min(down, diagonal);
             }
+            prev = curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
