f1729bd291e106a62ca11ba7e87771d36aebd81d
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-03-01 01:26:30 +0530

    Time: 14 ms (79.88%), Space: 10.3 MB (59.79%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,67 +1,71 @@
 class Solution {
 public:
     // memoization
-//     int solve(int n, int m, vector<vector<int>> &matrix,vector<vector<int>> &dp){
-//         // column might go negative or column might exceed the last index
-//         if(m<0 || m>= matrix[0].size()){
-//             return 1e9;
-//         }
-//         // destination
-//         if(n==0){
-//             return matrix[n][m];
-//         }
-//         if(dp[m][n]!=-1){
-//             return dp[n][m];
-//         }
+    int solve(int n, int m, vector<vector<int>> &matrix,vector<vector<int>> &dp){
+        // column might go negative or column might exceed the last index
+        if(m<0 || m>= matrix[0].size()){
+            return 1e9;
+        }
+        // destination
+        if(n==0){
+            return matrix[n][m];
+        }
+        if(dp[m][n]!=-1){
+            return dp[n][m];
+        }
              
-//         int up = matrix[n][m] + solve(n-1,m,matrix,dp);
-//         int diagleft = matrix[n][m] + solve(n-1,m-1,matrix,dp);
-//         int diagright = matrix[n][m] + solve(n-1,m+1,matrix,dp);
+        int up = matrix[n][m] + solve(n-1,m,matrix,dp);
+        int diagleft = matrix[n][m] + solve(n-1,m-1,matrix,dp);
+        int diagright = matrix[n][m] + solve(n-1,m+1,matrix,dp);
         
-//         return dp[n][m] = min({up,diagleft,diagright});
-//     }
+        return dp[n][m] = min({up,diagleft,diagright});
+    }
     
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        
         int n = matrix.size();
         int m = matrix[0].size();
 
-        vector<vector<int>> dp(n,vector<int>(m,0));
+        // space optimization
+
+        vector<int> prev(m,0);
         // base case
+        
         for(int j=0; j<m; j++){
-            dp[0][j] = matrix[0][j];
+            prev[j] = matrix[0][j];
         }
     
         
         for(int i=1; i<n; i++){
+            vector<int> curr(m,0);
             for(int j=0;j<m;j++){
                 
-                int up = matrix[i][j] + dp[i-1][j];
+                int up = matrix[i][j] + prev[j];
                 
                 int diagright = 0;
                 int diagleft = 0;
                
                 if(j-1>=0){
-                    diagleft = matrix[i][j] + dp[i-1][j-1];
+                    diagleft = matrix[i][j] + prev[j-1];
                 } 
                 else{
                     diagleft += 1e9;
                 }
                 
                 if(j+1<m){
-                    diagright = matrix[i][j] + dp[i-1][j+1];
+                    diagright = matrix[i][j] + prev[j+1];
                 } 
                 else{
                     diagright += 1e9;
                 }
                 
-                dp[i][j] = min({up,diagleft,diagright});
+                curr[j] = min({up,diagleft,diagright});
             }
+            prev = curr;
         }
         
-        int mini = dp[n-1][0];
+        int mini = prev[0];
         for(int j=1;j<m;j++){
-            mini = min(mini,dp[n-1][j]);
+            mini = min(mini,prev[j]);
         }
         
         return mini;
