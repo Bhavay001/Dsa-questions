5792217ce83e672d377a81ffcff1b103e2a54a74
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-03-19 01:20:36 +0530

    Time: 50 ms (34.39%), Space: 18.4 MB (50.00%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -3,7 +3,7 @@ public:
     
     int solve(int ind,vector<int> &coins, int amount,vector<vector<int>>&dp){
         if(ind==0){
-            // if amount is divisible by arr[0] it means we can get the answer 
+        // if amount is divisible by arr[0] it means we can get the answer 
            if(amount % coins[ind] ==0){
                return 1;
            } 
@@ -23,7 +23,24 @@ public:
     }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return solve(n-1,coins,amount,dp);
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        
+        for(int i = 0;i<=amount;i++){
+            dp[0][i] = (i % coins[0] ==0);
+        } 
+
+        for(int ind =1;ind<n;ind++){
+            for(int target = 0;target<=amount;target++){
+                int notpick = dp[ind-1][target];
+                int pick = 0;
+                // while picking we can pick it one more time so we stay at the same index
+                if(coins[ind]<=target){
+                    pick = dp[ind][target-coins[ind]];
+                }
+                // sum of all possibilities gives us the count
+                dp[ind][target] = pick + notpick;
+            }
+        }
+        return dp[n-1][amount];
     }
 };
