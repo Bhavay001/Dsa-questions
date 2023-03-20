49bcbc4f20a339828d37487aa4f91f5d402d729c
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-03-20 17:56:12 +0530

    Time: 83 ms (13.30%), Space: 24.5 MB (9.67%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -25,7 +25,25 @@ public:
         int n = text1.size();
         int m = text2.size();
         // memoization the depending parameters were ind1 and ind2 so we make dp of size dp[n][m]
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,text1,text2,dp); 
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        // shifting all indexes to 1 le
+        // when j is ==0
+        for(int i =0;i<=n;i++){
+            dp[i][0] = 0;
+        }
+        // when i ==0
+        for(int j =0;j<=m;j++){
+            dp[0][j] = 0;
+        }
+        for(int ind1 =1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                if(text1[ind1-1]==text2[ind2-1]){  
+                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                }
+                // not matching we move either ind1 or ind2 and return the maximum among them
+                else dp[ind1][ind2] = max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+            }
+        }
+        return dp[n][m]; 
     }
 };
