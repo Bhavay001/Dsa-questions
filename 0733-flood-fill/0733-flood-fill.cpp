35df279fe3decb8349325789986ed34e81519d57
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-03-24 02:05:36 +0530

    Time: 3 ms (98.90%), Space: 14.3 MB (28.84%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,28 +1,36 @@
 class Solution {
-    private:
-    void dfs(int row,int col,vector<vector<int>>& image,int color,int num,vector<int>&dx,vector<int>&dy){
-        image[row][col]= color;
-        int n = image.size();
-        int m = image[0].size();
-        for(int i =0;i<4;i++){
-            int newrow = row + dx[i];
-            int newcol = col + dy[i];
-            if(newrow>=0 && newrow<n && newcol>=0&& newcol < m && image[newrow][newcol] == num){
-                dfs(newrow,newcol,image,color,num,dx,dy);
-            }
-        }
-    }
 public:
+    // bfs
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        // int n = image.size();
-        // int m = image[0].size();
+        
         if(image[sr][sc] == color){
             return image;
         }
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        int prevcolor  = image[sr][sc];
+        image[sr][sc] = color;
+        int n = image.size();
+        int m = image[0].size();
+        
         vector<int> dx = {-1,1,0,0};
         vector<int> dy = {0,0,-1,1};
-        int num = image[sr][sc];
-        dfs(sr,sc,image,color,num,dx,dy);
+        
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            
+            for(int i =0;i<4;i++){
+                int newrow = row + dx[i];
+                int newcol = col + dy[i];
+                
+                if(newrow>=0 && newrow <n && newcol>=0 && newcol<m && image[newrow][newcol] == prevcolor ) {
+                    image[newrow][newcol] = color;
+                    q.push({newrow,newcol});
+                }
+            }
+        }
         return image;
     }
 };
