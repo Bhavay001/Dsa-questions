92ecca29743c76e934c529643e0b476210c2a15e
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-04-10 11:58:17 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (12.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    bool check(char ch2, char ch1){
+    bool check(char ch1,char ch2){
         if(ch1 == '(' && ch2 == ')' || ch1 == '[' && ch2 == ']' || ch1 == '{' && ch2 == '}'){
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
     }
     bool isValid(string s) {
         stack<int> st;
-        int i =0;
-        while(i<s.length()){
-            if(st.empty()){
+        for(int i =0;i<s.size();i++){
+            if(s[i]=='(' || s[i] == '[' || s[i]=='{'){
                 st.push(s[i]);
-                i++;
             }
             else{
-                if(check(s[i],st.top())){
-                    while(!st.empty() && check(s[i],st.top())){
-                        st.pop();
-                        i++;
-                    }
+                if(st.size()==0){
+                    return false;
+                }
+                char ch = st.top();
+                if(check(ch,s[i])){
+                    st.pop();
                 }
                 else{
-                    st.push(s[i]);
-                        i++;
+                    return false;
                 }
             }
         }
-        if(!st.empty()){
+        if(st.size()>0){
             return false;
         }
-        else{
-            return true;
-        }
+        return true;
     }
 };
