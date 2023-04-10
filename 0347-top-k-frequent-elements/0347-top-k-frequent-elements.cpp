ce21581c6e42b6a3c9c3c666f5d9da83b8b59a6e
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-04-10 13:00:01 +0530

    Time: 19 ms (52.35%), Space: 13.9 MB (28.07%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
-    static bool cmp(pair<int,int> &a,pair<int,int> &b){
-        return a.second>b.second;
-    }
-    vector<int> Sort(map<int,int> &mp,int k){
-        vector<pair<int,int>> v;
-        
-        for(auto x:mp){
-            v.push_back(x);
-        }
-        
-        sort(v.begin(),v.end(),cmp);
-        
-        vector<int> ans;
-        for(auto x:v){
-            if(k>0){
-                ans.push_back(x.first);
-                k--;
-            }
-            else{
-                break;
-            } 
-        }
-
-        return ans;
-        
-    }
-    
+    // priority queue solution
     vector<int> topKFrequent(vector<int>& nums, int k) {
         map<int,int> mp;
         for(int i =0;i<nums.size();i++){
             mp[nums[i]]++;
         }
-        return Sort(mp,k);
+        priority_queue<pair<int,int>,vector<pair<int,int>> ,greater<pair<int,int>>> pq;
+        for(auto x :mp){
+            pq.push({x.second,x.first});
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        vector<int> ans;
+        
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return ans;
     }
 };
