d54f8fc1f7d8a089b89db547fef97e1135275066
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-04-14 16:59:54 +0530

    Time: 135 ms (62.09%), Space: 73.1 MB (13.66%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,47 +1,33 @@
 class Solution {
 public:
     
-    int solve(int ind1,int ind2 , string &s1, string &s2, vector<vector<int>>&dp){
-        if(ind1<0 || ind2<0){
+    int solve(int i ,int j , string &s1, string &s2,vector<vector<int>> &dp){
+        if(i<0 || j<0){
             return 0;
         }
-        if(dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
+        
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
         // matching
-        if(s1[ind1]== s2[ind2]){
-            return dp[ind1][ind2] = 1 + solve(ind1-1,ind2-1,s1,s2,dp);
+        if(s1[i]== s2[j]){
+            return dp[i][j] = 1+ solve(i-1,j-1,s1,s2,dp);
         }
-        else return dp[ind1][ind2] = max(solve(ind1-1,ind2,s1,s2,dp),solve(ind1,ind2-1,s1,s2,dp));
+        // not matching
+        return dp[i][j] = max(solve(i-1,j,s1,s2,dp),solve(i,j-1,s1,s2,dp));
     }
     int longestPalindromeSubseq(string s) {
-        // longest palindromic of substring can be found by finding lcs of the give string and string
-        // formed by reversing it
+    // longest palindromic of substring can be found by finding lcs of the give string and string
+    // formed by reversing it
+        
         string s1 = s;
         string s2 = s;
+        
         reverse(s2.begin(),s2.end());
         int n = s1.size();
-        int m = s2.size();
-        vector<vector<int>> dp (n+1,vector<int>(m+1,-1));
         
-        for(int i =0;i<=n;i++){
-            dp[i][0] = 0;
-        }
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return solve(n-1,n-1,s1,s2,dp);
         
-        for(int j=0;j<=m;j++){
-            dp[0][j] = 0;
-        }
-        
-        for(int ind1=1;ind1<=n;ind1++){
-            for(int ind2=1;ind2<=m;ind2++){
-                if(s1[ind1-1]== s2[ind2-1]){
-                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
-                    
-                }
-                else dp[ind1][ind2] = max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
-            }
-        }
-               
-        return dp[n][m];
     }
 };
