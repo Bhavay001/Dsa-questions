bf0e351e665b2ff99e71f09f6118bc09bdfaaf47
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-04-22 12:43:53 +0530

    Time: 140 ms (5.54%), Space: 28.6 MB (24.45%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     
- 
+    int solve(string &s1, string &s2, int i, int j, vector<vector<int>> &dp){
+        if (i <0 || j< 0){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        // matching
+        if(s1[i]==s2[j]){
+            return dp[i][j] = 1 + solve(s1,s2,i-1,j-1,dp);
+        }
+        // not matching
+        else return dp[i][j] = max(solve(s1,s2,i-1,j,dp),solve(s1,s2,i,j-1,dp));
+    }
+    
     int minInsertions(string s) {
         // we will find the longest palidromic subsequence from the string 
         //s1 = mbadm
@@ -14,27 +28,11 @@ public:
         string s2 = s;
         reverse(s2.begin(),s2.end());
         int n = s1.length();
-        int m = s2.length();
-        vector<vector<int>> dp (n+1,vector<int>(m+1,-1));
         
-        for(int i =0;i<=n;i++){
-            dp[i][0] = 0;
-        }
+        vector<vector<int>> dp(n,vector<int>(n,-1));
         
-        for(int j=0;j<=m;j++){
-            dp[0][j] = 0;
-        }
+        int lps = solve(s1,s2,n-1,n-1,dp);
         
-        for(int ind1=1;ind1<=n;ind1++){
-            for(int ind2=1;ind2<=m;ind2++){
-                 if(s1[ind1-1]==s2[ind2-1]){
-                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
-                 }
-                 else dp[ind1][ind2] = max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
-        
-            }
-        }
-        int length = dp[n][m];
-        return n  - length;
+        return n- lps;
     }
 };
