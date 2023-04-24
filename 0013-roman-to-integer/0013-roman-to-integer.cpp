746a78af6076dbee4f178e068ee106bc8c3ca7e8
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-04-25 01:43:32 +0530

    Time: 15 ms (59.42%), Space: 8.3 MB (38.91%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int romanToInt(string s) {
         map<char,int> mp{
-            {'I', 1},
-            {'V', 5},
-            {'X', 10},
-            {'L', 50},
-            {'C', 100},
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
             {'D',500},
-            {'M',1000}
+            {'M',1000},
         };
-        int ans = 0;
-        for(int i =0;i<s.size()-1;i++){
-            if(mp[s[i+1]] <= mp[s[i]]){
-                ans += mp[s[i]];
+        int n = s.size();
+        int sum = 0;
+        for(int i =0;i<n-1;i++){
+            if(mp[s[i]]<mp[s[i+1]]){
+                sum -= mp[s[i]];
             }
             else{
-                ans -= mp[s[i]];
+                sum +=mp[s[i]];
             }
         }
-        ans += mp[s[s.size()-1]];
-        return ans;
-      
+        sum += mp[s[n-1]];
+        return sum;
     }
 };
