7a634a9092d14e58958b411865346f4e33f84cf1
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-04-25 20:38:30 +0530

    Time: 120 ms (77.11%), Space: 49.7 MB (63.94%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,21 +4,24 @@ public:
         if(nums.size()==0){
             return 0;
         }
-        sort(nums.begin(),nums.end());
-        int currlen = 1;
+        unordered_set<int> st;
+        for(int i =0;i<nums.size();i++){
+            st.insert(nums[i]);
+        }
+        // we try to count length only when we have found the starting point of the increasing sequence 
+        //if we calculate at every number it will be same as the brute force
         int maxlen = 1;
-        for(int i =0;i<nums.size()-1;i++){
-            if(nums[i]== nums[i+1]){
-                continue;
-            }
-            if(nums[i+1] == nums[i]+1){
-                currlen++;
-                maxlen= max(maxlen,currlen);
-            }
-            else{
-                currlen = 1;
+        for(auto x:st){
+            if(st.find(x-1) == st.end()){
+                int len = 1;
+                int num = x;
+                while(st.find(num+1)!=st.end()){
+                    num++;
+                    len++;
+                }
+                maxlen = max(maxlen,len);
             }
         }
-        return maxlen;
+        return  maxlen;
     }
 };
