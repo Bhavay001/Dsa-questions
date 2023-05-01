f656b3575f5c2046d057e8b7eab68c87d52f902b
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-05-01 16:15:46 +0530

    Time: 47 ms (30.35%), Space: 20.8 MB (33.23%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
-    /* Consider example 1 : strs = ["eat","tea","tan","ate","nat","bat"]
-            we will like this in map   
-            aet -- eat, tea, ate
-            ant -- tan, nat
-            abt -- bat */
-        
-        unordered_map<string,vector<string>> mp;
+        map<string,vector<string>> mp;
         
         for(int i =0;i<strs.size();i++){
             string s = strs[i];
-            sort(s.begin(),s.end());
-            
-            mp[s].push_back(strs[i]);
+            string s1 = strs[i];
+            sort(s1.begin(),s1.end());
+            mp[s1].push_back(s);
         }
         
         vector<vector<string>> ans;
         for(auto x:mp){
             ans.push_back(x.second);
         }
-        
         return ans;
     }
 };
