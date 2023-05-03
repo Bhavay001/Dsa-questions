f00732be9276f7a655ad32d202f349d6eee8c3f5
Bhavay Singhal <108589333+Bhavay001@users.noreply.github.com>
2023-05-03 14:52:13 +0530

    Time: 147 ms (27.08%), Space: 93.3 MB (55.31%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        vector<int> maxprofit;
         int n = prices.size();
-        // make a prefix max subarray
-        vector<int> prefix(n,0);
-        prefix[n-1] = prices[n-1];
-        for(int i = n-2;i>=0;i--){
-            prefix [i] = max(prefix[i+1],prices[i]);
-        }
         int maxi = 0;
-        for(int i =0;i<n;i++){
-            maxi = max(maxi ,prefix[i] - prices[i]);
+         int ans = 0;
+        for(int i = n-1;i>=0;i--){
+            maxi = max(maxi,prices[i]);
+            ans = max(maxi - prices[i],ans);  
+        
         }
-        return maxi;
+        
+      
+        return ans;
     }
 };
